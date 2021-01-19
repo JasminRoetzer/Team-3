7ca47b4964d3d90e0464e1897b12ab8b41c310b5
amelie99 <amelie01@outlook.de>
2021-01-19 10:58:59 +0100

    Data Merge

---
+++ Script_datamerge.R
@@ -0,0 +1,32 @@
+
+
+
+library(readxl)  ##load Excel Data
+Umsatzdaten_Excel <- read_excel("raw_data/Umsatzdaten_Excel.xlsx")
+
+Wetter_Excel <- read_excel("raw_data/Wetter_Excel.xlsx")
+
+Schiffsverkehr <- read_excel("raw_data/Schiffsverkehr.xlsx")
+
+Uebernachtungen_Kiel_Mai_August_2019_new <- read_excel("raw_data/Uebernachtungen_Kiel_Mai-August_2019_new.xlsx")
+
+Besucher_Kiwo <- read_excel("raw_data/Besucher_Kiwo.xlsx")
+
+kiwo_Excel <- read_excel("raw_data/kiwo_Excel.xlsx")
+
+
+## merging
+
+m1 <- merge(x=Umsatzdaten_Excel,y= Wetter_Excel,by= "Datum", all.x=TRUE) ## merge Umsatzdaten mit Wetter
+
+m2 <- merge(x=m1, y= Schiffsverkehr, by="Datum", all.x = TRUE) ## merge m1 mit Schiffsverkehr
+
+m3 <- merge(x=m2, y= Uebernachtungen_Kiel_Mai_August_2019_new, all.x = TRUE) ## merge m2 mit Übernachtungen
+
+m4 <- full_join(x=m3, y= Besucher_Kiwo) ## merge m3 mit Besucherzahlen
+
+m5 <- full_join(x= m4, y= kiwo_Excel) ## merge m4 mit Kiwo
+
+
+library(plyr)
+m_final <- ddply(m5,"Datum",numcolwise(sum)) ## merge Datumsangaben
